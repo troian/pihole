917f736e48b0588ec5af0d58a48a3d12dc264278
Artur Troian <troian.ap@gmail.com>
2023-01-28 10:57:03 -0500

    feat: add tailscale
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- etc/cont-init.d/1-init.sh
+++ etc/cont-init.d/1-init.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-mkdir -p /run/sshd
+if [[ ${SSHD_ENABLE:=false} == true ]]; then
+    mkdir -p /run/sshd
+fi
+
+if [[ ${TAILSCALE_ENABLE:=false} == true ]]; then
+    mkdir -p /var/run/tailscale /var/cache/tailscale /var/lib/tailscale
+fi
--- etc/cont-init.d/10-dote.sh
+++ etc/cont-init.d/10-dote.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+# shellcheck disable=SC2086
 /opt/dote $DOTE_OPTS -d
